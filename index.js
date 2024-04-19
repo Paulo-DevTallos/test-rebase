cec118609991951d7f91e53aa128f0973fa73ae8
Paulo Sergio <paulo.sergio@tallos.com.br>
2024-04-19 14:27:20 -0300

    commit feaat2

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 class Execute {
     name = ""
     lastname = ""
+    age = null;
     
-    constructor(name, lastname) {
+    constructor(name, lastname, age) {
         this.name = name;
         this.lastname = lastname;
+        this.age = age;
     }
 
     mountFullName() {
         return `${this.name} ${this.lastname}`
     }
+
+    getAge() {
+        return this.age;
+    }
 }
 
 const execute = new Execute("John", "Doe")
 console.log(execute.mountFullName());
+
+
+const executeClass = new Execute(32)
+console.log(executeClass.getAge()); 
