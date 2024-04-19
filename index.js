52788bdc62b7b3b63f8e67a7441364c77684f97a
Paulo Sergio <paulo.sergio@tallos.com.br>
2024-04-19 14:20:28 -0300

    test commit 1

---
+++ index.js
@@ -0,0 +1,14 @@
+class Execute {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        console.log('Execute');
+    }
+}
+
+
+
+const executeClass = new Execute()
+executeClass.init(); 
